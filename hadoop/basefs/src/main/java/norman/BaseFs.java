d0e55cb9105ef80da43d1be0ad19c7aca6c03261
gsbeegnnord <gsbeegnnord@icloud.com>
2018-04-30 12:33:43 +0800

    add list/del/meta

--- hadoop/basefs/src/main/java/norman/BaseFs.java
+++ hadoop/basefs/src/main/java/norman/BaseFs.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
@@ -23,6 +24,10 @@ public class BaseFs {
         FileSystem fs = FileSystem.get(URI.create(uri), conf);
         InputStream in = null;
         try {
+            if (!fs.exists(new Path(uri))) {
+                System.err.println(uri + " is not exists");
+                System.exit(-1);
+            }
             in = fs.open(new Path(uri));
             IOUtils.copyBytes(in, System.out, 4096, false);
         } finally {
@@ -36,13 +41,43 @@ public class BaseFs {
         // example, uri: /x/y/z, fs will combine conf and path into hdfs://localhost:9000/x/y/z
         FileSystem fs = FileSystem.get(URI.create(dst), conf);
         InputStream in = new BufferedInputStream(new FileInputStream(src));
+        // create will auto create parent directory(if it do not exist)
+        // also FileSystem can use mkdirs to create a directory explicity
         OutputStream out = fs.create(new Path(dst));
         IOUtils.copyBytes(in, out, 4096, true);
     }
 
+    private void getMetaInfo(String uri) throws Exception {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        FileStatus fileStatus = fs.getFileStatus(new Path(uri));
+        System.out.println("is directory: " + fileStatus.isDirectory());
+        System.out.println("path: " + fileStatus.getPath());
+        System.out.println("replication: " + fileStatus.getReplication());
+        System.out.println("owner: " + fileStatus.getOwner());
+    }
+
+    private void listFiles(String uri) throws Exception {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        // can add PathFilter to filter result
+        FileStatus[] fileStatuses = fs.listStatus(new Path(uri));
+        for (FileStatus fileStatus: fileStatuses) {
+            System.out.println("path in " + uri + ": " + fileStatus.getPath());
+        }
+    }
+
+    private void delFile(String uri) throws Exception {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        boolean res = fs.delete(new Path(uri), false);
+        if (res)
+            System.out.println("delete " + uri + " success");
+    }
+
     public static void main(String[] args) throws Exception {
-        if (args.length != 3) {
-            System.err.println("need path arg: readData source destination");
+        if (args.length != 4) {
+            System.err.println("need path arg: readData source destination metaFile");
             System.exit(-1);
         }
 
@@ -50,5 +85,8 @@ public class BaseFs {
 
         baseFs.copyData(args[0]);
         baseFs.createFile(args[1], args[2]);
+        baseFs.getMetaInfo(args[0]);
+        baseFs.listFiles(args[3]);
+        baseFs.delFile(args[2]);
     }
 }
